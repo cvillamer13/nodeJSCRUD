c6cdbe2ad6b4f3f70d25ceac3d8b9bb1e4b11b3c
Alden Vallestero <82009290+aldenvallestero@users.noreply.github.com>
2023-04-13 21:11:51 +0800

    refactor: reduce unwanted line

--- index.js
+++ index.js
@@ -1,93 +1,71 @@
-const mysql = require('mysql');
 const express = require('express');
+const mysql = require('mysql');
 var app = express();
-const bodyparser = require('body-parser');
 
-
-app.use(bodyparser.json());
+app.use(express.json());
 
 
 var mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'NODEDB'
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 });
 
-mysqlConnection.connect((err)=>{
-    if(!err){
-        console.log('DB Connection Success!');
-    }else{
-        console.log('DB Connection Falied! \n Error :' + JSON.stringify(err, undefined, 2));
-    }
+mysqlConnection.connect((err) => {
+    if(!err) console.log('DB Connection Success!');
+    else console.log('DB Connection Falied! \n Error :' + JSON.stringify(err, undefined, 2));
 });
 
-
-
-app.listen(3000, ()=>console.log('Server Running'));
-
-
-//Get All data
-app.get('/Users', (req,res)=>{
-    mysqlConnection.query('SELECT * FROM test', (err, rows, fields)=>{
-        if(!err){
-            res.send(rows);
-        }else{
-            console.log(err);
-        }
+app.get('/users', (req, res) => {
+    mysqlConnection.query('SELECT * FROM test', (err, rows, fields) => {
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
     });
 });
 
-
-//Get 1 data
-app.get('/Users/:id', (req,res)=>{
-    mysqlConnection.query('SELECT * FROM test WHERE id = ?', [req.params.id], (err, rows, fields)=>{
-        if(!err){
-         res.send(rows);
-        }else{
-            console.log(err);
-        }
+app.get('/users/:id', (req, res)=>{
+    mysqlConnection.query('SELECT * FROM test WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
     });
 });
 
-
-//Delete 1 data
-app.delete('/Users/:id', (req,res)=>{
-    mysqlConnection.query('DELETE FROM test WHERE id = ?', [req.params.id], (err, rows, fields)=>{
-        if(!err){
-            res.send('Deleted Success');
-        }else{
-            console.log(err);
-        }
+app.delete('/users/:id', (req, res) => {
+    mysqlConnection.query('DELETE FROM test WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if(!err)
+        res.send('Deleted Success');
+        else
+        console.log(err);
     });
 });
 
-
-//Insert 1 data
-app.post('/Users', (req,res)=>{
+app.post('/users', (req, res) => {
     const UserName = req.body.UserName;
     const EmailAdd = req.body.EmailAdd;
     
-    mysqlConnection.query('INSERT INTO test (UserName, EmailAdd) VALUES (?,?)', [UserName, EmailAdd], (err, rows, fields)=>{
-        if(!err){
-            res.send('Insert Success');
-        }else{
-            console.log(err);
-        }
+    mysqlConnection.query('INSERT INTO test (UserName, EmailAdd) VALUES (?,?)', [UserName, EmailAdd], (err, rows, fields) => {
+        if(!err)
+        res.send('Insert Success');
+        else
+        console.log(err);
     });
 });
 
-
-//update 1 data
-app.put('/Users', (req,res)=>{
+app.put('/users', (req, res) => {
     const UserName = req.body.UserName;
     const EmailAdd = req.body.EmailAdd;
     
-    mysqlConnection.query('UPDATE test SET EmailAdd = ? WHERE UserName = ?', [EmailAdd, UserName], (err, rows, fields)=>{
-        if(!err){
-            res.send('updated Success');
-        }else{
-            console.log(err);
-        }
+    mysqlConnection.query('UPDATE test SET EmailAdd = ? WHERE UserName = ?', [EmailAdd, UserName], (err, rows, fields) => {
+        if(!err)
+        res.send('updated Success');
+        else
+        console.log(err);
     });
 });
+
+app.listen(3000, () => console.log('Server Running'));
